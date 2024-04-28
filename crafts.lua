5a2d3a58a5a1bf33415f371bc4d0a2836617779e
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2024-04-28 08:59:59 -0500

    Add files via upload

---
+++ crafts.lua
@@ -0,0 +1,8 @@
+		minetest.register_craft({
+			output = "better_roads:stonebrick_desertsand 2",
+			recipe = {
+				{"default:desert_sand", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
---
+++ init.lua
@@ -0,0 +1,324 @@
+
+
+
+minetest.register_node("better_roads:stonebrick_desertsand", {
+	description = "Stonebrick And DesertSand",
+	paramtype2 = "facedir",
+	tiles = {"default_desert_sand.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_desert_sand.png",
+		"default_desert_sand.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_coniferous_litter", {
+	description = "Stonebrick And Coniferous Litter",
+	paramtype2 = "facedir",
+	tiles = {"default_coniferous_litter.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dirt.png^default_coniferous_litter_side.png",
+		"default_dirt.png^default_coniferous_litter_side.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_coalblock", {
+	description = "Stonebrick And Coalblock",
+	paramtype2 = "facedir",
+	tiles = {"default_coal_block.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_coal_block.png",
+		"default_coal_block.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_desertcobble", {
+	description = "Stonebrick And Desert Cobble Stone",
+	paramtype2 = "facedir",
+	tiles = {"default_desert_cobble.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_desert_cobble.png",
+		"default_desert_cobble.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_desert_sandstone", {
+	description = "Stonebrick And Desert Sandstone",
+	paramtype2 = "facedir",
+	tiles = {"default_desert_sandstone.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_desert_sandstone.png",
+		"default_desert_sandstone.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_desert_stone", {
+	description = "Stonebrick And Desert Stone",
+	paramtype2 = "facedir",
+	tiles = {"default_desert_stone.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_desert_stone.png",
+		"default_desert_stone.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_dirt", {
+	description = "Stonebrick And Dirt",
+	paramtype2 = "facedir",
+	tiles = {"default_dirt.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dirt.png",
+		"default_dirt.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_dry_dirt", {
+	description = "Stonebrick And Dry Dirt",
+	paramtype2 = "facedir",
+	tiles = {"default_dry_dirt.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dry_dirt.png",
+		"default_dry_dirt.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_dry_dirt_with_dry_grass", {
+	description = "Stonebrick And Dry Dirt With Dry Grass ",
+	paramtype2 = "facedir",
+	tiles = {"default_dry_grass.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dry_dirt.png^default_dry_grass_side.png",
+		"default_dry_dirt.png^default_dry_grass_side.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_dirt_with_grass", {
+	description = "Stonebrick And Dirt With Grass",
+	paramtype2 = "facedir",
+	tiles = {"default_grass.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dirt.png^default_grass_side.png",
+		"default_dirt.png^default_grass_side.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_gravel", {
+	description = "Stonebrick And Gravel",	
+	paramtype2 = "facedir",
+	tiles = {"default_gravel.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_gravel.png",
+		"default_gravel.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_ice", {
+	description = "Stonebrick And Ice",
+	paramtype2 = "facedir",
+	tiles = {"default_ice.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_ice.png",
+		"default_ice.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_permatfrost_with_moss", {
+	description = "Stonebrick And Permafrost With Moss",
+	paramtype2 = "facedir",
+	tiles = {"default_moss.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_permafrost.png^default_moss_side.png",
+		"default_permafrost.png^default_moss_side.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_obsidian", {
+	description = "Stonebrick And Obsidian",
+	paramtype2 = "facedir",
+	tiles = {"default_obsidian.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_obsidian.png",
+		"default_obsidian.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_permafrost", {
+	description = "Stonebrick And Permafrost",
+	paramtype2 = "facedir",
+	tiles = {"default_permafrost.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_permafrost.png",
+		"default_permafrost.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_lava", {
+	description = "Stonebrick And Lava",
+	paramtype2 = "facedir",
+	paramtype = "light",
+	light_source = default.LIGHT_MAX - 1,
+	tiles = {
+		{
+			name = "stone_brick_lava_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3.0,
+			},
+		},
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		{
+			name = "default_lava_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3.0,
+			},
+		},
+		{
+			name = "default_lava_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3.0,
+			},
+		},
+		"default_stone_brick.png",
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_rainforest_litter", {
+	description = "Stonebrick And Rainforest Litter",
+	paramtype2 = "facedir",
+	tiles = {"default_rainforest_litter.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_dirt.png^default_rainforest_litter_side.png",
+		"default_dirt.png^default_rainforest_litter_side.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_sand", {
+	description = "Stonebrick And Sand",
+	paramtype2 = "facedir",
+	tiles = {"default_sand.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_sand.png",
+		"default_sand.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_sandstone", {
+	description = "Stonebrick And Sandstone",
+	paramtype2 = "facedir",
+	tiles = {"default_sandstone.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_sandstone.png",
+		"default_sandstone.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_silver_sand", {
+	description = "Stonebrick And Silver Sand",
+	paramtype2 = "facedir",
+	tiles = {"default_silver_sand.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_silver_sand.png",
+		"default_silver_sand.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_silver_sandstone", {
+	description = "Stonebrick And Silver SandStone",
+	paramtype2 = "facedir",
+	tiles = {"default_silver_sandstone.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_silver_sandstone.png",
+		"default_silver_sandstone.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_node("better_roads:stonebrick_snow", {
+	description = "Stonebrick And Snow",
+	paramtype2 = "facedir",
+	tiles = {"default_snow.png^overlay.png",
+		"default_stone_brick.png",
+		"default_stone_brick.png",
+		"default_snow.png",
+		"default_snow.png",
+		"default_stone_brick.png"
+		},
+	groups = {cracky = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+local modname = minetest.get_current_modname()
+local modpath = minetest.get_modpath(modname)
+
+dofile(modpath.."/crafts.lua")
