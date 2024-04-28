1c58ec4cb9f946da133f96e5a7da04305f16b612
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2024-04-28 09:35:17 -0500

    Finish adding all crafting recipes

--- crafts.lua
+++ crafts.lua
@@ -1,7 +1,176 @@
-		minetest.register_craft({
+
+	minetest.register_craft({
+			output = "better_roads:stonebrick_coalblock 2",
+			recipe = {
+				{"default:coalblock", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+	minetest.register_craft({
+			output = "better_roads:stonebrick_coniferous_litter 2",
+			recipe = {
+				{"default:dirt_with_coniferous_litter", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+	minetest.register_craft({
+			output = "better_roads:stonebrick_desert_sandstone 2",
+			recipe = {
+				{"default:desert_sandstone", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+	minetest.register_craft({
+			output = "better_roads:stonebrick_desert_stone 2",
+			recipe = {
+				{"default:desert_stone", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+	minetest.register_craft({
+			output = "better_roads:stonebrick_desertcobble 2",
+			recipe = {
+				{"default:desert_cobble", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+	minetest.register_craft({
 			output = "better_roads:stonebrick_desertsand 2",
 			recipe = {
 				{"default:desert_sand", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_dirt 2",
+			recipe = {
+				{"default:dirt", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_dirt_with_grass 2",
+			recipe = {
+				{"default:dirt_with_grass", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_dry_dirt 2",
+			recipe = {
+				{"default:dry_dirt", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_dry_dirt_with_dry_grass 2",
+			recipe = {
+				{"default:dry_dirt_with_grass", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_gravel 2",
+			recipe = {
+				{"default:gravel", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_ice 2",
+			recipe = {
+				{"default:ice", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_lava 2",
+			recipe = {
+				{"bucket:bucket_lava", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_obsidian 2",
+			recipe = {
+				{"default:obsidian", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_permafrost 2",
+			recipe = {
+				{"default:permafrost", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_permafrost_with_moss 2",
+			recipe = {
+				{"default:permafrost_with_moss", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_rainforest_litter 2",
+			recipe = {
+				{"default:dirt_with_rainforest_litter", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_sand 2",
+			recipe = {
+				{"default:sand", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_sandstone 2",
+			recipe = {
+				{"default:sandstone", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_silver_sand 2",
+			recipe = {
+				{"default:silver_sand", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_silver_sandstone 2",
+			recipe = {
+				{"default:silver_sandstone", "default:stonebrick", ""},
+				{"", "", ""},
+				{"", "", ""}
+			}
+		})
+		minetest.register_craft({
+			output = "better_roads:stonebrick_snow 2",
+			recipe = {
+				{"default:snowblock", "default:stonebrick", ""},
 				{"", "", ""},
 				{"", "", ""}
 			}
